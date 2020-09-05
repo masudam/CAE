0a17f7cab27ec06555b8608beaa588afe3045aba
Masayuki Masuda <masudam224@gmail.com>
2020-09-05 23:51:59 +0900

    first commmit

---
+++ main_tf2.py
@@ -0,0 +1,129 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Reshape, Conv2DTranspose
+from tensorflow.keras import Model
+
+# おまじない（自分の環境では必要）
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
+class CAE(Model):
+    def __init__(self):
+        super(CAE,self).__init__()
+        # 使用するブロックを宣言しておく
+        self.encoder = tf.keras.Sequential([
+            Conv2D(16, 3, strides=(2,2), padding='same', activation=tf.nn.tanh), # 16x16x16
+            Conv2D(32, 3, strides=(2,2), padding='same', activation=tf.nn.tanh), # 8x8x32
+            Conv2D(32, 3, strides=(2,2), padding='same', activation=tf.nn.tanh), # 4x4x32
+            Flatten(),
+            Dense(4*4*8, activation=tf.nn.tanh), # 128 dim.
+        ])
+        self.decoder = tf.keras.Sequential([
+            Dense(4*4*32, activation=tf.nn.tanh),
+            Reshape((4,4,32)),
+            Conv2DTranspose(32, 3, strides=2, padding='same', activation=tf.nn.tanh), # 8x8x32
+            Conv2DTranspose(16, 3, strides=2, padding='same', activation=tf.nn.tanh), # 16x16x16
+            Conv2DTranspose(3, 3, strides=2, padding='same', activation=tf.nn.sigmoid), # 32x32x3
+        ])
+
+    def call(self, x):
+        z = self.encoder(x)
+        x_pred = self.decoder(z)
+        return x_pred, z
+
+
+
+# モデルの途中結果を見て保存する関数
+def generate_and_save_images(model, epoch, test_sample):
+    predictions, _ = model(test_sample)
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(predictions[i, :, :, :])
+        plt.axis('off')
+
+    plt.savefig('./result_tf2/image_at_epoch_{:04d}.png'.format(epoch))
+
+# kerasにあるcifar10のデータセットを用意する関数
+def prepare_data(train_size, test_size, batch_size):
+    (train_images, _), (test_images, _) = tf.keras.datasets.cifar10.load_data()
+    def preprocess_images(images):
+        images = images.reshape((images.shape[0], 32, 32, 3)) / 255.
+        return images.astype('float32')
+
+    train_images = preprocess_images(train_images)
+    test_images = preprocess_images(test_images)
+
+    train_dataset = (tf.data.Dataset.from_tensor_slices(train_images)
+                    .shuffle(train_size).batch(batch_size))
+    test_dataset = (tf.data.Dataset.from_tensor_slices(test_images)
+                    .shuffle(test_size).batch(batch_size))
+
+    return train_dataset, test_dataset
+
+
+if __name__ == "__main__":
+    # Training Parameters
+    train_size = 40000
+    batch_size = 32
+    test_size = 10000
+
+    epochs = 30
+    lr = 0.0001
+
+    # prepare data
+    train_dataset, test_dataset = prepare_data(train_size, test_size, batch_size)
+
+    # テストデータのサンプル元データの保存
+    num_examples_to_generate = 16
+    assert batch_size >= num_examples_to_generate
+    for test_batch in test_dataset.take(1):
+        test_sample = test_batch[0:num_examples_to_generate, :, :, :]
+
+    fig = plt.figure(figsize=(4, 4))
+    for i in range(test_sample.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(test_sample[i, :, :, :])
+        plt.axis('off')
+    plt.savefig('./result_tf2/test_sample.png')
+
+
+    model = CAE()
+    optimizer = tf.keras.optimizers.Adam(lr)
+
+    generate_and_save_images(model, 0, test_sample)
+
+    def calc_loss(model, x):
+        x_pred, _ = model(x)
+        loss = tf.reduce_mean(tf.pow(x-x_pred, 2))
+        return loss
+
+    for epoch in range(1, epochs + 1):
+        start_time = time.time()
+        for train_x in train_dataset:
+            with tf.GradientTape() as tape:
+                loss = calc_loss(model, train_x)
+            grad = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grad, model.trainable_variables))
+        end_time = time.time()
+
+        test_loss = tf.keras.metrics.Mean()
+        for test_x in test_dataset:
+            test_loss(calc_loss(model, test_x))
+        elbo = test_loss.result()
+        print('Epoch: {}, Train set loss: {}, Test set loss: {}, time elapse for current epoch: {}'
+                .format(epoch, loss, elbo, end_time - start_time))
+        if epoch % 5 == 0:
+            generate_and_save_images(model, epoch, test_sample)
---
+++ main_torch.py
@@ -0,0 +1,127 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data.dataset import Subset
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+# viewをsequentialの中で使えるようにするためモジュールにする
+class Reshape(nn.Module):
+    def __init__(self, *args):
+        super(Reshape, self).__init__()
+        self.shape = args
+    def forward(self,x):
+        return x.view(self.shape)
+
+def get_torch_vars(x):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+
+
+class CAE(nn.Module):
+    def __init__(self):
+        super(CAE, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1), nn.Tanh(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1), nn.Tanh(),
+            nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1), nn.Tanh(),
+            nn.Flatten(),
+            nn.Linear(4*4*32, 128), nn.Tanh(),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(128, 4*4*32), nn.Tanh(),
+            Reshape(-1, 32, 4, 4),
+            nn.ConvTranspose2d(32, 32, kernel_size=3, stride=2, padding=1, output_padding=1), nn.Tanh(),
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1), nn.Tanh(),
+            nn.ConvTranspose2d(16, 3, kernel_size=3, stride=2, padding=1, output_padding=1), nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        x_pred = self.decoder(z)
+        return x_pred, z
+
+# モデルの途中結果を見て保存する関数
+def generate_and_save_images(pic, epoch):
+    fig = plt.figure(figsize=(4, 4))
+    for i in range(16):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(pic.cpu().data[i, :, :, :].permute(1, 2, 0))
+        plt.axis('off')
+    if epoch == 0:
+        plt.savefig('./result_torch/test_sample.png')
+    else:
+        plt.savefig('./result_torch/image_at_epoch_{:04d}.png'.format(epoch))
+
+
+def prepare_data(batch_size):
+    transform = torchvision.transforms.ToTensor()
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    n_sample = len(trainset)
+    train_size = int(n_sample * 0.8)
+    print("train data: {}, validation data: {}".format(train_size, n_sample-train_size))
+    subset1_indices = list(range(0,train_size))
+    subset2_indices = list(range(train_size,n_sample))
+    train_dataset = Subset(trainset, subset1_indices)
+    val_dataset   = Subset(trainset, subset2_indices)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    data_loaders = {"train": train_loader, "val": val_loader}
+    return data_loaders
+
+if __name__ == "__main__":
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # Training Parameters
+    batch_size = 32
+    epochs = 10
+    lr = 0.0001
+
+    # prepare data
+    data_loaders = prepare_data(batch_size)
+    for inputs, _ in data_loaders['val']:
+        inputs = get_torch_vars(inputs)
+        generate_and_save_images(inputs,0)
+        break
+
+    # create model
+    model = CAE().to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters())
+
+    def calc_loss(x,loss_func):
+        x = get_torch_vars(x)
+        x_pred, _ = model(x)
+        loss = loss_func(x, x_pred)
+        return loss, x_pred
+
+    for epoch in range(1, epochs+1):
+        start_time = time.time()
+        losses = {'train': [], 'val': []}
+        for phase in ['train', 'val']:
+            for i, (inputs, _) in enumerate(data_loaders[phase]):
+                if phase == 'train':
+                    model.train()
+                    loss, _ = calc_loss(inputs, criterion)
+
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                elif phase == 'val':
+                    model.eval()
+                    loss, x_pred = calc_loss(inputs, criterion)
+                    if i == 0 and epoch % 5 == 0:
+                        generate_and_save_images(x_pred, epoch)
+
+                losses[phase].append(loss.data.item())
+
+        end_time = time.time()
+        print('Epoch: {}, Train set loss: {}, validation set loss: {}, time elapse for current epoch: {}'
+                .format(epoch, np.array(losses['train']).mean(), np.array(losses['val']).mean(), end_time - start_time))
